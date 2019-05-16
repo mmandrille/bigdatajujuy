3bd7dbc47565a595a759e49b8a20a70777cc0d92
Marcos Federico Mandrille <mmandrille@gmail.com>
2019-05-16 17:56:58 -0300

    Se sigue trabajando en el bug

--- bigdatajujuy/calendario/functions.py
+++ bigdatajujuy/calendario/functions.py
@@ -6,16 +6,19 @@ from .models import Evento
 
 #Definimos funciones
 def prox_evento():
-    try:  evento = Evento.objects.filter(fecha_inicio__gte=timezone.now()).order_by('fecha_inicio').first()
-    except Evento.DoesNotExist: return "Sin Eventos(Hora Actual)", timezone.now()
-    return evento.get_tipo_display(), evento.fecha_inicio
+    try: 
+        evento = Evento.objects.filter(fecha_inicio__gte=timezone.now()).order_by('fecha_inicio').first()
+        return evento.get_tipo_display(), evento.fecha_inicio
+    except Evento.DoesNotExist or AttributeError:
+        return "Sin Eventos(Hora Actual)", timezone.now()
 
 def duracion_congreso():
     try: 
         duracion = Evento.objects.get(tipo=2).fecha_inicio - Evento.objects.get(tipo=1).fecha_inicio 
         if duracion.seconds > 0:
             duracion = duracion.days + 1
-        else: duracion = duracion.days
-        return duracion
-    except Evento.DoesNotExist: 
+        else: 
+            duracion = duracion.days
+    except Evento.DoesNotExist or AttributeError: 
         duracion = "0"
+    return duracion
