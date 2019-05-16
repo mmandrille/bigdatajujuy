03a08cb2571db4d5b7290d35b05fe406d73c4190
Marcos Federico Mandrille <mmandrille@gmail.com>
2019-05-16 18:53:08 -0300

    Se arreglo bug de eventos y se volvio dinamico el countdown

--- bigdatajujuy/calendario/functions.py
+++ bigdatajujuy/calendario/functions.py
@@ -6,11 +6,13 @@ from .models import Evento
 
 #Definimos funciones
 def prox_evento():
+    #Formato de entrega: "6 13, 2019 11:00:00"
     try: 
-        evento = Evento.objects.filter(fecha_inicio__gte=timezone.now()).order_by('fecha_inicio').first()
-        return evento.get_tipo_display(), evento.fecha_inicio
-    except Evento.DoesNotExist: return "Sin Eventos(Hora Actual)", timezone.now()
-    except AttributeError: return "Sin Eventos(Hora Actual)", timezone.now()
+        prox_fecha = Evento.objects.filter(fecha_inicio__gte=timezone.now()).order_by('fecha_inicio').first().fecha_inicio
+    except:
+        prox_fecha = timezone.now()
+    str_prox_fecha = prox_fecha.strftime("%m %d, %Y %H:%M:%S")
+    return str_prox_fecha
 
 def duracion_congreso():
     try: 
@@ -19,6 +21,6 @@ def duracion_congreso():
             duracion = duracion.days + 1
         else: 
             duracion = duracion.days
-    except Evento.DoesNotExist: duracion = "0"
-    except AttributeError: duracion = "0"
+    except: 
+        duracion = "0"
     return duracion
